95c30e0475e3dbc203dc167365cbd55bbbefc0fe
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2025-08-14 10:37:11 +0200

    Edited api path

--- src/pages/api/getAllPosts.tsx
+++ src/pages/api/getAllPosts.tsx
@@ -1,6 +1,6 @@
 
 export default async function getAllPosts() {
-    const res = await fetch(`https://noroff.tnjensen.com/wp-json/wp/v2/posts?categories=6,9&_embed&filter[orderby]=date&order=asc`, {
+    const res = await fetch(`https://noroff.tnjensen.com/wp-json/portfolio/wp/v2/posts?categories=6,9&_embed&filter[orderby]=date&order=asc`, {
         next: {
             revalidate: 10,
         }
